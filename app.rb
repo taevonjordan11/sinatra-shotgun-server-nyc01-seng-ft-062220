2121409b0b37e484a6b328877f275af06a826e17
Taevon Jordan <nude.plus@MacBook-Pro.home>
2020-07-13 10:00:29 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! "
+  get '/' do
+    "Welcome to your app, Taevon. You are now using shotgun!!!! "
   end
 
 end
